69a785f05378035ed9dc0a528aabe04038df3bbb
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-11 01:56:26 +0600

    new code

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,22 +1,22 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import {getAuth} from 'firebase/auth'
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBuZoJhaEWMB2aX6ZDsAd8j-0UVODpWMzM",
-  authDomain: "genius-car-services-e9c83.firebaseapp.com",
-  projectId: "genius-car-services-e9c83",
-  storageBucket: "genius-car-services-e9c83.appspot.com",
-  messagingSenderId: "282755348839",
-  appId: "1:282755348839:web:138ac800eb65895af797c2"
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBuZoJhaEWMB2aX6ZDsAd8j-0UVODpWMzM",
+//   authDomain: "genius-car-services-e9c83.firebaseapp.com",
+//   projectId: "genius-car-services-e9c83",
+//   storageBucket: "genius-car-services-e9c83.appspot.com",
+//   messagingSenderId: "282755348839",
+//   appId: "1:282755348839:web:138ac800eb65895af797c2"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app)
+// const auth = getAuth(app)
 
-export default auth
+// export default auth
